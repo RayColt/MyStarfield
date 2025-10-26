a8062ebb9d55c7c99d86dfab71036b740a2e6967
raycolt <ray_colt@outlook.com>
2025-10-26 12:46:26 +0100

    upd

--- MyStarfield/MyStarfield.cpp
+++ MyStarfield/MyStarfield.cpp
@@ -279,13 +279,6 @@ static void RenderFrame(RenderWindow* rw, float dt, float totalTime)
                 Ellipse(rw->backHdc, ix - psz, iy - psz, ix + psz + 1, iy + psz + 1);
                 SelectObject(rw->backHdc, oldBrush);
             }
-            /* Old if (brushes[bucket]){}/Ellipse part with 2px stars
-                    HBRUSH oldBrush = (HBRUSH)SelectObject(rw->backHdc, brushes[bucket]);
-                    Ellipse(rw->backHdc,
-                        (int)floorf(px - psz), (int)floorf(py - psz),
-                        (int)ceilf(px + psz + 1), (int)ceilf(py + psz + 1));
-                    SelectObject(rw->backHdc, oldBrush);
-            */
         }
     }
 
